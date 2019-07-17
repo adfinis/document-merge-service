d5dcb82445047fa16e7d861f047fc538a9f53060
Oliver Sauder <sliverc@users.noreply.github.com>
2019-07-17 15:24:41 +0200

    Allow search lookup to be used in sqlite (#147)
    For this to work we simply fallback to icontains.
    Mysql and Postgres will still use their full text search.
    
    This is a prequisite to get django 2.2 running.

--- document_merge_service/api/apps.py
+++ document_merge_service/api/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.db.models import TextField
+from django.db.models.lookups import IContains
 
 
 class DefaultConfig(AppConfig):
     name = "document_merge_service.api"
+
+    def ready(self):
+        if "sqlite3" in settings.DATABASES["default"]["ENGINE"]:  # pragma: no cover
+            TextField.register_lookup(IContains, lookup_name="search")
