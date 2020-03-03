de810446fbec2ffe610cda4f9cb12be34b5bdbb5
David Vogt <david.vogt@adfinis-sygroup.ch>
2020-03-03 17:45:05 +0100

    feat(validation): validate docx template on upload
    Docx templates can contain syntax errors that will only get caught when
    rendering the template.  This causes problems, as the user seeing the
    problem (some frontend user for example) may not be the same person who
    caused the problem (the person creating the template).
    
    Thus we now validate the syntax on upload already, as far as we can by
    trying to render the templates on upload (works for both engines).
    
    * Validation for the mailmerge engine ensures that the template has
      *some* placeholders.
    * Validation for the docx template engine validates the syntax as well.

--- document_merge_service/api/engines.py
+++ document_merge_service/api/engines.py
@@ -2,6 +2,7 @@ import zipfile
 
 from docx import Document
 from docxtpl import DocxTemplate
+from jinja2.exceptions import TemplateSyntaxError
 from mailmerge import MailMerge
 from rest_framework import exceptions
 
@@ -10,18 +11,38 @@ from .jinja import get_jinja_env
 
 
 class DocxValidator:
-    def validate(self):
+    def _validate_is_docx(self):
         try:
             Document(self.template)
         except (ValueError, zipfile.BadZipfile):
             raise exceptions.ParseError("not a valid docx file")
+        finally:
+            self.template.seek(0)
 
-        self.template.seek(0)
+    def validate_template_syntax(self):  # pragma: no cover
+        raise NotImplementedError(
+            "validate_template_syntax must be implemented in engine class"
+        )
+
+    def validate(self):
+        self._validate_is_docx()
+        self.validate_template_syntax()
 
 
 class DocxTemplateEngine(DocxValidator):
     def __init__(self, template):
         self.template = template
+
+    def validate_template_syntax(self):
+        try:
+            doc = DocxTemplate(self.template)
+            doc.render({"foo": "bar"})
+        except TemplateSyntaxError as exc:
+            arg_str = ";".join(exc.args)
+            raise exceptions.ValidationError(f"Syntax error in template: {arg_str}")
+
+        finally:
+            self.template.seek(0)
 
     def merge(self, data, buf):
         doc = DocxTemplate(self.template)
@@ -34,6 +55,20 @@ class DocxTemplateEngine(DocxValidator):
 class DocxMailmergeEngine(DocxValidator):
     def __init__(self, template):
         self.template = template
+
+    def _has_placeholders(self, part):
+        return bool(part.findall("//MergeField"))
+
+    def validate_template_syntax(self):
+        document = MailMerge(self.template)
+        # syntax can't be invalid as it's validated by office
+        # suites. However we need to have *some* placeholders
+        has_placeholders = any(
+            self._has_placeholders(part) for part in document.parts.values()
+        )
+        self.template.seek(0)
+        if not has_placeholders:
+            raise exceptions.ValidationError("Template has no merge fields")
 
     def merge(self, data, buf):
         with MailMerge(self.template) as document:
