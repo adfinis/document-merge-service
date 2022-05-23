faeb7759f9c4e7028c3ec5fba589faf144b7055c
Jean-Louis Fuchs <jean-louis.fuchs@adfinis.com>
2022-05-23 15:27:51 +0200

    chore(poetry): add initial pyproject.toml

--- document_merge_service/document_merge_service_metadata.py
+++
@@ -1,7 +0,0 @@
-"""Used to single sourcing metadata about document_merge_service."""
-
-__title__ = "document_merge_service"
-__description__ = "Merge Document Template Service"
-__version__ = "4.6.2"
-__author__ = "Adfinis AG"
-__url__ = "https://github.com/adfinis-sygroup/document-merge-service"
