448087728f3103744b8245ff5400b63201352b19
Jonas Metzener <jonas.metzener@adfinis.com>
2023-07-24 11:09:46 +0200

    fix(template): migrate group to meta property before removing

--- document_merge_service/api/migrations/0006_remove_template_group.py
+++ document_merge_service/api/migrations/0006_remove_template_group.py
@@ -3,12 +3,30 @@
 from django.db import migrations
 
 
+def migrate_group_to_meta(apps, schema_editor):
+    Template = apps.get_model("api", "Template")
+
+    for template in Template.objects.filter(group__isnull=False):
+        template.meta["group"] = template.group
+        template.save()
+
+
+def migrate_group_to_meta_reverse(apps, schema_editor):
+    Template = apps.get_model("api", "Template")
+
+    for template in Template.objects.filter(meta__has_key="group"):
+        template.group = template.meta["group"]
+        del template.meta["group"]
+        template.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("api", "0005_xlsx_template_engine"),
     ]
 
     operations = [
+        migrations.RunPython(migrate_group_to_meta, migrate_group_to_meta_reverse),
         migrations.RemoveField(
             model_name="template",
             name="group",
